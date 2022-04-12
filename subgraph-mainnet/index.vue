45d61d9d6b71811ca6cd6d19514629b371c39687
Emma-Jane MacKinnon-Lee <72898465+emmajane1313@users.noreply.github.com>
2022-04-12 16:50:50 -0400

    add

--- subgraph-mainnet/index.vue
+++ subgraph-mainnet/index.vue
@@ -1,7 +1,7 @@
 {
   "network": "mainnet",
-  "CypherKeyAuctionHouse": "0x53d77078650d2d34f3478C3b7D6E6736D4c946E7",
-    "CypherKeyAuctionHouseStartBlock": 27061357
-  "CypherKeyToken": "0x8A34e2045a315Cc1f45979397e47bF21F9e87e8E",
-    "CypherKeyTokenStartBlock": 27061345
+  "CypherKeyAuctionHouse": "0xE3fCaD485CE184C1b48930915396a9f0b98C86F3",
+    "CypherKeyAuctionHouseStartBlock": 14572268
+  "CypherKeyToken": "0xb60E0F5ac61723c80a00e88DcD93746c921E30a9",
+    "CypherKeyTokenStartBlock": 14572262
 }
